d16a0ee6b1f4300ad8e96b907aa31a667ee3b1e6
David Horton <61114342+DavesCodeMusings@users.noreply.github.com>
2024-09-25 09:57:52 -0500

    Update proof_of_concept.ino

--- proof_of_concept.ino
+++ proof_of_concept.ino
@@ -104,8 +104,8 @@ void loop() {
       // batteryLevelCharacteristic.writeValue(<batteryPercent>);
       // where <batteryPercent> is an unsigned char value between 0 and 100.
 
-      // Delay between updates. (Don't make too long or connections start to timeout.)
-      delay(1000);
+      delay(100);  // Throttle updates, balancing reponsiveness vs. CPU usage.
+      
       if (sleepCountdown <= 0) {
         Serial.print("Sleeping for: ");
         Serial.println(SLEEPING_TIME_SECS);
